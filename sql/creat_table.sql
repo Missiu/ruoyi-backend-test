740f3d35fd7e1730d23a27136a53ae1737fc2de1
Missiu <15775278810@163.com>
2024-06-16 12:38:19 +0800

    ls04 -修改数据库信息为合理值

--- sql/creat_table.sql
+++ sql/creat_table.sql
@@ -67,12 +67,12 @@ CREATE TABLE `active_evaluation`
 DROP TABLE IF EXISTS `active_file`;
 CREATE TABLE `active_file`
 (
-    `file_id`           INT                                                                               NOT NULL AUTO_INCREMENT COMMENT '文件ID',
-    `work_id`           INT                                                                               NOT NULL COMMENT '作品ID',
-    `file_category`     ENUM ('视频', '教案', '教学实施报告', '专业人才培养方案', '课程标准', '其他附件') NOT NULL COMMENT '文件类别',
-    `file_name`         VARCHAR(255)                                                                      NOT NULL COMMENT '文件名',
-    `file_storage_path` VARCHAR(255)                                                                      NOT NULL COMMENT '文件存储路径',
-    `uploaded_at`       DATETIME                                                                          NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '上传时间',
+    `file_id`           INT          NOT NULL AUTO_INCREMENT COMMENT '文件ID',
+    `work_id`           INT          NOT NULL COMMENT '作品ID',
+    `file_category`     VARCHAR(64)  NOT NULL COMMENT '文件类别',
+    `file_name`         VARCHAR(255) NOT NULL COMMENT '文件名',
+    `file_storage_path` VARCHAR(255) NOT NULL COMMENT '文件存储路径',
+    `uploaded_at`       DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '上传时间',
     PRIMARY KEY (`file_id`),
     INDEX `idx_work_id_file_id` (`work_id`, `file_id`),
     CONSTRAINT `fk_active_file_works` FOREIGN KEY (`work_id`) REFERENCES `active_works` (`work_id`) ON DELETE CASCADE
@@ -81,30 +81,46 @@ CREATE TABLE `active_file`
 # 插入示例数据
 INSERT INTO `active_user` (`real_name`, `school_name`, `user_type`, `group_name`, `judge_scope`, `login_name`,
                            `password`, `create_by`)
-VALUES ('张三', '北京大学', '1', '1', '1', 'zhangsan', 'hashed_password1', 'system'),
-       ('李四', '清华大学', '2', '2', '2', 'lisi', 'hashed_password2', 'system'),
-       ('王五', '复旦大学', '2', '3', '3', 'wangwu', 'hashed_password3', 'system'),
-       ('赵六', '浙江大学', '1', '4', '4', 'zhaoliu', 'hashed_password4', 'system');
+VALUES ('张三', '北京大学', '1', '', '2', 'zhangsan', 'hashed_password1', 'system'),
+       ('李四', '清华大学', '2', '2', '', 'lisi', 'hashed_password2', 'system'),
+       ('王五', '复旦大学', '2', '3', '', 'wangwu', 'hashed_password3', 'system'),
+       ('赵六', '浙江大学', '1', '', '3', 'zhaoliu', 'hashed_password4', 'system');
 INSERT INTO `active_works` (`work_name`, `school_name`, `participant`, `contact_info`, `group_name`)
-VALUES ('智能助手设计', '北京大学', '张三', '12345678901', '1'),
-       ('数据挖掘技术', '清华大学', '李四', '23456789012', '2'),
-       ('机器学习算法', '复旦大学', '王五', '34567890123', '3'),
-       ('深度学习应用', '浙江大学', '赵六', '45678901234', '4');
+VALUES ('数据挖掘技术', '清华大学', '李四', '23456789012', '2'),
+       ('机器学习算法', '复旦大学', '王五', '34567890123', '3');
 INSERT INTO `active_evaluation` (`work_id`, `judge_name`, `score_teaching_implementation`, `score_lesson_plan`,
                                  `score_video_material`,
                                  `score_talent_training`, `score_course_standard`, `score_teaching_material`,
                                  `final_score`)
-VALUES (1, '评委A', 90.00, 92.00, 91.00, 93.00, 94.00, 95.00, 92.80),
-       (2, '评委B', 85.00, 88.00, 87.00, 86.00, 89.00, 90.00, 87.40),
-       (3, '评委C', 78.00, 80.00, 79.00, 81.00, 82.00, 83.00, 80.00),
-       (4, '评委D', 88.00, 90.00, 89.00, 91.00, 92.00, 93.00, 90.20);
+VALUES (2, '张三', 85.00, 88.00, 87.00, 86.00, 89.00, 90.00, 87.40),
+       (3, '赵六', 78.00, 80.00, 79.00, 81.00, 82.00, 83.00, 80.00);
 INSERT INTO `active_file` (`work_id`, `file_category`, `file_name`, `file_storage_path`)
-VALUES (1, '视频', '智能助手设计视频.mp4', '/path/to/video/1.mp4'),
-       (1, '教案', '智能助手设计教案.pdf', '/path/to/teaching_plan/1.pdf'),
-       (2, '视频', '数据挖掘技术视频.mp4', '/path/to/video/2.mp4'),
-       (2, '教学实施报告', '数据挖掘技术实施报告.docx', '/path/to/implementation_report/2.docx'),
-       (3, '视频', '机器学习算法视频.mp4', '/path/to/video/3.mp4'),
-       (4, '课程标准', '深度学习应用课程标准.doc', '/path/to/course_standard/4.doc');
+VALUES (2, '作品图片', '作品图片.png', '../uploadPath/active/2/作品图片.png'),
+       (2, '视频', '视频1.mkv', '../uploadPath/active/2/videos/视频1.mkv'),
+       (2, '视频', '视频2.mkv', '../uploadPath/active/2/videos/视频2.mkv'),
+       (2, '视频', '视频3.mkv', '../uploadPath/active/2/videos/视频3.mkv'),
+       (2, '视频', '视频4.mkv', '../uploadPath/active/2/videos/视频4.mkv'),
+       (2, '教案', '教案.docx', '../uploadPath/active/2/教案.docx'),
+       (2, '教学实施报告', '教学实施报告.pdf', '../uploadPath/active/2/教学实施报告.pdf'),
+       (2, '专业人才培养方案', '专业人才培养方案.docx', '../uploadPath/active/2/专业人才培养方案.docx'),
+       (2, '课程标准', '课程标准.pdf', '../uploadPath/active/2/课程标准.pdf'),
+       (2, '其他附件', '其他附件1.pdf', '../uploadPath/active/2/other/其他附件1.pdf'),
+       (2, '其他附件', '其他附件2.pdf', '../uploadPath/active/2/other/其他附件2.pdf'),
+       (2, '其他附件', '其他附件3.pdf', '../uploadPath/active/2/other/其他附件3.pdf'),
+
+       (3, '作品图片', '作品图片.png', '../uploadPath/active/3/作品图片.png'),
+       (3, '视频', '视频1.mkv', '../uploadPath/active/3/videos/视频1.mkv'),
+       (3, '视频', '视频2.mkv', '../uploadPath/active/3/videos/视频2.mkv'),
+       (3, '视频', '视频3.mkv', '../uploadPath/active/3/videos/视频3.mkv'),
+       (3, '视频', '视频4.mkv', '../uploadPath/active/3/videos/视频4.mkv'),
+       (3, '教案', '教案.docx', '../uploadPath/active/3/教案.docx'),
+       (3, '教学实施报告', '教学实施报告.pdf', '../uploadPath/active/3/教学实施报告.pdf'),
+       (3, '专业人才培养方案', '专业人才培养方案.docx', '../uploadPath/active/3/专业人才培养方案.docx'),
+       (3, '课程标准', '课程标准.pdf', '../uploadPath/active/3/课程标准.pdf'),
+       (3, '其他附件', '其他附件1.pdf', '../uploadPath/active/3/other/其他附件1.pdf'),
+       (3, '其他附件', '其他附件2.pdf', '../uploadPath/active/3/other/其他附件2.pdf'),
+       (3, '其他附件', '其他附件3.pdf', '../uploadPath/active/3/other/其他附件3.pdf');
+
 
 -- ----------------------------
 -- 若依生成: 账号管理模块sql
@@ -186,24 +202,36 @@ values ('作品导出', @parentId, '5', '#', '', 1, 0, 'F', '0', '0', 'active:wo
 -- 若依生成: 评价信息模块sql
 -- ----------------------------
 -- 菜单 SQL
-insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status, perms, icon, create_by, create_time, update_by, update_time, remark)
-values('作品评价', '2000', '1', 'evaluation', 'active/evaluation/index', 1, 0, 'C', '0', '0', 'active:evaluation:list', '#', 'admin', sysdate(), '', null, '作品评价菜单');
+insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status,
+                      perms, icon, create_by, create_time, update_by, update_time, remark)
+values ('作品评价', '2000', '1', 'evaluation', 'active/evaluation/index', 1, 0, 'C', '0', '0', 'active:evaluation:list',
+        '#', 'admin', sysdate(), '', null, '作品评价菜单');
 
 -- 按钮父菜单ID
 SELECT @parentId := LAST_INSERT_ID();
 
 -- 按钮 SQL
-insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status, perms, icon, create_by, create_time, update_by, update_time, remark)
-values('作品评价查询', @parentId, '1',  '#', '', 1, 0, 'F', '0', '0', 'active:evaluation:query',        '#', 'admin', sysdate(), '', null, '');
+insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status,
+                      perms, icon, create_by, create_time, update_by, update_time, remark)
+values ('作品评价查询', @parentId, '1', '#', '', 1, 0, 'F', '0', '0', 'active:evaluation:query', '#', 'admin',
+        sysdate(), '', null, '');
 
-insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status, perms, icon, create_by, create_time, update_by, update_time, remark)
-values('作品评价新增', @parentId, '2',  '#', '', 1, 0, 'F', '0', '0', 'active:evaluation:add',          '#', 'admin', sysdate(), '', null, '');
+insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status,
+                      perms, icon, create_by, create_time, update_by, update_time, remark)
+values ('作品评价新增', @parentId, '2', '#', '', 1, 0, 'F', '0', '0', 'active:evaluation:add', '#', 'admin', sysdate(),
+        '', null, '');
 
-insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status, perms, icon, create_by, create_time, update_by, update_time, remark)
-values('作品评价修改', @parentId, '3',  '#', '', 1, 0, 'F', '0', '0', 'active:evaluation:edit',         '#', 'admin', sysdate(), '', null, '');
+insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status,
+                      perms, icon, create_by, create_time, update_by, update_time, remark)
+values ('作品评价修改', @parentId, '3', '#', '', 1, 0, 'F', '0', '0', 'active:evaluation:edit', '#', 'admin', sysdate(),
+        '', null, '');
 
-insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status, perms, icon, create_by, create_time, update_by, update_time, remark)
-values('作品评价删除', @parentId, '4',  '#', '', 1, 0, 'F', '0', '0', 'active:evaluation:remove',       '#', 'admin', sysdate(), '', null, '');
+insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status,
+                      perms, icon, create_by, create_time, update_by, update_time, remark)
+values ('作品评价删除', @parentId, '4', '#', '', 1, 0, 'F', '0', '0', 'active:evaluation:remove', '#', 'admin',
+        sysdate(), '', null, '');
 
-insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status, perms, icon, create_by, create_time, update_by, update_time, remark)
-values('作品评价导出', @parentId, '5',  '#', '', 1, 0, 'F', '0', '0', 'active:evaluation:export',       '#', 'admin', sysdate(), '', null, '');
+insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status,
+                      perms, icon, create_by, create_time, update_by, update_time, remark)
+values ('作品评价导出', @parentId, '5', '#', '', 1, 0, 'F', '0', '0', 'active:evaluation:export', '#', 'admin',
+        sysdate(), '', null, '');
